56ff557210eb00aa991feedf84cfd40f749ec5f6
Cris <cristianarellanoagudo@gmail.com>
2024-05-26 16:43:47 +0200

    Hay que pagar.....

---
+++ lib/presentation/chat/screens/chats_barril.dart
@@ -0,0 +1,6 @@
+export 'package:trip_planner/presentation/chat/pages/calls_page.dart';
+export 'package:trip_planner/presentation/chat/pages/notifications_page.dart';
+export 'package:trip_planner/presentation/chat/pages/contacts_page.dart';
+export 'package:trip_planner/presentation/chat/pages/messages_page.dart';
+export 'package:trip_planner/presentation/chat/screens/chat_screenAPI.dart';
+export 'select_user_screen.dart';
---
+++ lib/presentation/chat/screens/profile_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+
+import '../../../app.dart';
+import '../widget/widgets.dart';
+import 'chats_barril.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static Route get route => MaterialPageRoute(
+        builder: (context) => const ProfileScreen(),
+      );
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = context.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Hero(
+              tag: 'hero-profile-picture',
+              child: Avatar.large(url: user?.image),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(user?.name ?? 'No name'),
+            ),
+            const Divider(),
+            const _SignOutButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SignOutButton extends StatefulWidget {
+  const _SignOutButton({
+    super.key,
+  });
+
+  @override
+  __SignOutButtonState createState() => __SignOutButtonState();
+}
+
+class __SignOutButtonState extends State<_SignOutButton> {
+  bool _loading = false;
+
+  Future<void> _signOut() async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      await StreamChatCore.of(context).client.disconnectUser();
+
+      Navigator.of(context).push(SelectUserScreen.route);
+    } on Exception catch (e, st) {
+      logger.e('Could not sign out');
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _loading
+        ? const CircularProgressIndicator()
+        : TextButton(
+            onPressed: _signOut,
+            child: const Text('Sign out'),
+          );
+  }
+}
